6ee4abc9b87e4e4e09c4a40b1653e79f76f7f6fb
dz_void <98200188+dz-void@users.noreply.github.com>
2023-01-20 16:20:36 +0300

    Updated link

--- 2  Programs/Installation batchfiles/Download & Install SearchEverything.bat
+++ 2  Programs/Installation batchfiles/Download & Install SearchEverything.bat
@@ -1,6 +1,6 @@
 @echo Downloading SearchEverything, please wait
 @echo off
-powershell Invoke-WebRequest "https://www.voidtools.com/Everything-1.4.1.1005.x64-Setup.exe" -OutFile "%temp%\SearchEverything_install.exe"
+powershell Invoke-WebRequest "https://www.voidtools.com/Everything-1.4.1.1022.x64-Setup.exe" -OutFile "%temp%\SearchEverything_install.exe"
 echo Download done, execution is imminent
 @echo off
 %temp%\SearchEverything_install.exe
