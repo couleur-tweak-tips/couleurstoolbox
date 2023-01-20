1b79d7f85b2b26dc5d88e1e1abced3319af7b404
dz_void <98200188+dz-void@users.noreply.github.com>
2023-01-20 16:21:02 +0300

    Updated link

--- 2  Programs/Installation batchfiles/Download & install VLC.bat
+++ 2  Programs/Installation batchfiles/Download & install VLC.bat
@@ -1,6 +1,6 @@
 @echo Downloading VLC, please wait
 @echo off
-powershell Invoke-WebRequest "https://get.videolan.org/vlc/3.0.14/win64/vlc-3.0.14-win64.exe" -OutFile "%temp%\VLC_install.exe"
+powershell Invoke-WebRequest "https://get.videolan.org/vlc/3.0.18/win64/vlc-3.0.18-win64.exe" -OutFile "%temp%\VLC_install.exe"
 echo Download done, execution is imminent
 @echo off
 %temp%\VLC_install.exe
