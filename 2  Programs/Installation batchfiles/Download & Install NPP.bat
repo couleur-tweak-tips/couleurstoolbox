dac9c2648f2ec9c96e060f55fc3d842a4136f509
dz_void <98200188+dz-void@users.noreply.github.com>
2023-01-20 16:18:37 +0300

    Updated link

--- 2  Programs/Installation batchfiles/Download & Install NPP.bat
+++ 2  Programs/Installation batchfiles/Download & Install NPP.bat
@@ -1,6 +1,6 @@
 @echo Downloading Notepad++, please wait
 @echo off
-powershell Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.5/npp.7.9.5.Installer.exe" -OutFile "%temp%\NPP_install.exe"
+powershell Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.8/npp.8.4.8.Installer.x64.exe" -OutFile "%temp%\NPP_install.exe"
 echo Download done, execution is imminent
 @echo off
 %temp%\NPP_install.exe
